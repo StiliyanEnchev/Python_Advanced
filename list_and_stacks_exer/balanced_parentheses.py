b0a62c019e06671c996a168784dc28fdb50f54c6
StiliyanEnchev <145556929+StiliyanEnchev@users.noreply.github.com>
2024-01-13 09:16:30 +0200

    Update Python Advanced

--- list_and_stacks_exer/balanced_parentheses.py
+++ list_and_stacks_exer/balanced_parentheses.py
@@ -1,17 +1,21 @@
 from collections import deque
 
 parentheses = deque(input())
+opening = ['[', '{', '(']
+closing = [']', '}', ')']
 
 while parentheses:
+
+    if len(parentheses) == 1:
+        break
+
     first, second = parentheses.popleft(), parentheses.pop()
-    if first == '[' and second != ']':
+
+    if first not in opening or second not in closing:
         print("NO")
         break
-    elif first == '{' and second != '}':
-        print("NO")
-        break
-    elif first == '(' and second != ')':
-        print("NO")
-        break
-else:
-    print('YES')
+        
+    for index in range(len(opening)):
+        if first == opening[index] and second == opening[index]:
+
+

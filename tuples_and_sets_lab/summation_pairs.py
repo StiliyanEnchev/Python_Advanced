87159c66fa1e5b4aff07dfbb1bb69b1936bda740
StiliyanEnchev <145556929+StiliyanEnchev@users.noreply.github.com>
2024-01-14 09:32:57 +0200

    Update Python Advanced

--- tuples_and_sets_lab/summation_pairs.py
+++ tuples_and_sets_lab/summation_pairs.py
@@ -1,8 +1,20 @@
+import time
+
+
 numbers = [int(x) for x in input().split()]
 result = int(input())
 
+start = time.time()
+second_list = numbers.copy()
+
 for num in range(len(numbers)):
     current_num = numbers.pop()
-    for index in range(len(numbers)):
-        if current_num + numbers[index] == result:
-            print(f"{current_num} + {numbers[index]} = {result}")
+    second_list.pop()
+    for index in range(len(second_list)):
+        second_num = second_list[index]
+
+        if current_num + second_num == result:
+            print(f"{current_num} + {second_num} = {result}")
+
+end = time.time()
+print(f'{end-start}')

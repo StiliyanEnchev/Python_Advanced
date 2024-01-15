dcd744c9299ea23125f33e21f00f91a0d07c4a65
StiliyanEnchev <145556929+StiliyanEnchev@users.noreply.github.com>
2024-01-15 20:12:36 +0200

    Update Python Advanced

--- tuples_and_sets_lab/summation_pairs.py
+++ tuples_and_sets_lab/summation_pairs.py
@@ -1,20 +1,23 @@
 import time
 
-
 numbers = [int(x) for x in input().split()]
 result = int(input())
-
 start = time.time()
-second_list = numbers.copy()
+numbers_to_remove = []
 
 for num in range(len(numbers)):
     current_num = numbers.pop()
-    second_list.pop()
+    second_list = numbers.copy()
+
     for index in range(len(second_list)):
         second_num = second_list[index]
-
+        found = False
         if current_num + second_num == result:
             print(f"{current_num} + {second_num} = {result}")
+            break
+
+
 
 end = time.time()
 print(f'{end-start}')
+print(numbers)

657e5b503d2873da9759ca80e7a0a2eca723917f
StiliyanEnchev <145556929+StiliyanEnchev@users.noreply.github.com>
2024-01-15 21:34:54 +0200

    Update Python Advanced

--- tuples_and_sets_exer/count_symbols.py
+++ tuples_and_sets_exer/count_symbols.py
@@ -1,11 +1,7 @@
-text = input()
 dictionary = {}
 
-for index in range(len(text)):
-    symbol = text[index]
-    if symbol not in dictionary:
-        dictionary[symbol] = 0
-    dictionary[symbol] += 1
+for symbol in input():
+    dictionary[symbol] = dictionary.get(symbol, 0) + 1
 
 for symb, number in sorted(dictionary.items()):
     print(f"{symb}: {number} time/s")

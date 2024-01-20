9b7cec2cf88e28d045d38d60813ce1fbce99c9a7
StiliyanEnchev <145556929+StiliyanEnchev@users.noreply.github.com>
2024-01-20 12:03:10 +0200

    Update Python Advanced

--- main.py
+++ main.py
@@ -1,11 +1,1 @@
-matrix = []
-row_num = int(input())
-sum = 0
-
-for index in range(row_num):
-    matrix.append([int(x) for x in input().split()])
-    sum += matrix[index][-1]
-
-print(sum)
-
-#reverse sum the diagonal
+print(range(3))

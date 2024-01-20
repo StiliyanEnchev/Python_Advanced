320b30e8d66d0ffe392c2236f2c2632d9f5b5232
StiliyanEnchev <145556929+StiliyanEnchev@users.noreply.github.com>
2024-01-20 09:59:25 +0200

    Update Python Advanced

--- multidimensional_lists_exer/diagonals.py
+++ multidimensional_lists_exer/diagonals.py
@@ -4,5 +4,5 @@ matrix = [[int(el) for el in input().split(', ')] for _ in range(n)]
 primary = [matrix[r][r] for r in range(n)]
 second = [matrix[r][n - r - 1] for r in range(n)]
 
-print(f'Primary diagonal: {', '.join(str(num) for num in primary)}. Sum: {sum(primary)}')
-print(f'Secondary diagonal: {', '.join(str(num) for num in second)}. Sum: {sum(second)}')
+print(f"Primary diagonal: {', '.join(str(num) for num in primary)}. Sum: {sum(primary)}")
+print(f"Secondary diagonal: {', '.join(str(num) for num in second)}. Sum: {sum(second)}")

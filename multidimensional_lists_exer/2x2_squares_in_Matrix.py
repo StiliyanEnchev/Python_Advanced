34d90f67521d06adc37b1086dad50e57d49837e2
StiliyanEnchev <145556929+StiliyanEnchev@users.noreply.github.com>
2024-01-20 11:39:29 +0200

    Update Python Advanced

--- multidimensional_lists_exer/2x2_squares_in_Matrix.py
+++ multidimensional_lists_exer/2x2_squares_in_Matrix.py
@@ -7,7 +7,7 @@ for row in range(rows - 1):
     for col in range(cols - 1):
         symbol = matrix[row][col]
 
-        if symbol == matrix[row][col+1] and symbol == matrix[row+1][col] and symbol == matrix[row+1][col+1]:
+        if symbol == matrix[row][col+1] == matrix[row+1][col] == matrix[row+1][col+1]:
             equal_matrixes += 1
 
 print(equal_matrixes)

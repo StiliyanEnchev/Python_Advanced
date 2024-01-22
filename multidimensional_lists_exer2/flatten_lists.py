b8a2e9fde27fc5b5e984eead9edf05cade1ccd31
StiliyanEnchev <145556929+StiliyanEnchev@users.noreply.github.com>
2024-01-22 18:18:42 +0200

    Update Python Advanced

--- multidimensional_lists_exer2/flatten_lists.py
+++ multidimensional_lists_exer2/flatten_lists.py
@@ -1,8 +1,2 @@
-line = input().split('|')
-
-sub_list = []
-
-for sub_string in line[::-1]:
-    sub_list.extend(sub_string.split())
-
-print(*sub_list)
+numbers = [string.split() for string in input().split('|')]
+print(*[' '.join(sublist) for sublist in numbers[::-1] if sublist])

bacdf2644e9998d4b583619d05d72817f947685b
StiliyanEnchev <145556929+StiliyanEnchev@users.noreply.github.com>
2024-01-22 18:12:52 +0200

    Update Python Advanced

--- stacks_queues_tuples_sets_exer/milkshakes.py
+++ stacks_queues_tuples_sets_exer/milkshakes.py
@@ -10,6 +10,7 @@ while chocolates and cups_of_milk:
     if chocolates[-1] <= 0:
         chocolates.pop()
         continue
+
     elif cups_of_milk[0] <= 0:
         cups_of_milk.popleft()
         continue
@@ -20,12 +21,17 @@ while chocolates and cups_of_milk:
 
     if current_choc == current_milk:
         milkshales += 1
+
         if milkshales == 5:
             print('Great! You made all the chocolate milkshakes needed!')
             break
+
     else:
         cups_of_milk.append(current_milk)
         chocolates.append(current_choc - 5)
+
+    if milkshales == 5:
+        break
 
 if milkshales < 5:
     print("Not enough milkshakes.")
@@ -39,6 +45,3 @@ if milkshales:
     print(f'Milk: {", ".join([str(number) for number in cups_of_milk])}')
 else:
     print("Milk: empty")
-
-# 20, 24, -5, 17, 22, 60, 26
-# 26, 60, 22, 17, 24, 10, 55

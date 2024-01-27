b6fde3a5ff915fbc78bb8ee6b94cd3d099bc62ec
StiliyanEnchev <145556929+StiliyanEnchev@users.noreply.github.com>
2024-01-27 10:14:05 +0200

    Update Python Advanced

--- main.py
+++ main.py
@@ -1,7 +1,2 @@
-def sum_nums(a, b, *args):
-    total_sum = a + b
-    for el in args:
-        total_sum += el
-    return total_sum
 
-print(sum_nums(1, 2, 1, 5))
+print(sorted(list, key=lambda x: x[0]))

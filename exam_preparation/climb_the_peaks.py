e64d7641f0d5df6b52514807a6a6f13524137c0d
StiliyanEnchev <145556929+StiliyanEnchev@users.noreply.github.com>
2024-02-13 17:09:11 +0200

    Update Python Advanced

---
+++ exam_preparation/climb_the_peaks.py
@@ -0,0 +1,27 @@
+from collections import deque
+
+food = [int(x) for x in input().split(", ")]
+stamina = deque([int(x) for x in input().split(", ")])
+
+conquered_peaks = []
+day = 0
+
+peaks_dict = {
+    "Vihren": 80,
+    "Kutelo": 90,
+    "Banski Suhodol": 100,
+    "Polezhan": 60,
+    "Kamenitza": 70,
+}
+
+
+
+if XXX:
+    print("Alex did it! He climbed all top five Pirin peaks in one week -> @FIVEinAWEEK")
+
+elif XXX:
+    print("Alex failed! He has to organize his journey better next time -> @PIRINWINS")
+
+if XXX:
+    print("Conquered peaks:")
+    print(*conquered_peaks, sep="\n")

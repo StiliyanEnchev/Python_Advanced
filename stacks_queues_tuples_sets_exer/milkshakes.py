7b0cc69787b03d8d4b35d7b27350ffcba49d1791
StiliyanEnchev <145556929+StiliyanEnchev@users.noreply.github.com>
2024-02-14 14:19:33 +0200

    Update Python Advanced

--- stacks_queues_tuples_sets_exer/milkshakes.py
+++ stacks_queues_tuples_sets_exer/milkshakes.py
@@ -1,11 +1,11 @@
 from collections import deque
 
-chocolates = deque(int(x) for x in input().split(', '))
+chocolates = [int(x) for x in input().split(', ')]
 cups_of_milk = deque(int(x) for x in input().split(', '))
 
-milkshales = 0
+milkshakes = 0
 
-while chocolates and cups_of_milk:
+while chocolates and cups_of_milk and milkshakes != 5:
 
     if chocolates[-1] <= 0:
         chocolates.pop()
@@ -15,33 +15,20 @@ while chocolates and cups_of_milk:
         cups_of_milk.popleft()
         continue
 
-    if cups_of_milk[0] <= 0:
-        cups_of_milk.popleft()
     current_choc, current_milk = chocolates.pop(), cups_of_milk.popleft()
 
     if current_choc == current_milk:
-        milkshales += 1
-
-        if milkshales == 5:
-            print('Great! You made all the chocolate milkshakes needed!')
-            break
+        milkshakes += 1
 
     else:
         cups_of_milk.append(current_milk)
         chocolates.append(current_choc - 5)
 
-    if milkshales == 5:
-        break
-
-if milkshales < 5:
+if milkshakes < 5:
     print("Not enough milkshakes.")
-
-if chocolates:
-    print(f'Chocolate: {", ".join([str(number) for number in chocolates])}')
 else:
-    print("Chocolate: empty")
+    print("Great! You made all the chocolate milkshakes needed!")
 
-if milkshales:
-    print(f'Milk: {", ".join([str(number) for number in cups_of_milk])}')
-else:
-    print("Milk: empty")
+
+print(f'Chocolate: {(", ".join([str(number) for number in chocolates])) if chocolates else "empty"}')
+print(f'Milk: {(", ".join([str(number) for number in cups_of_milk]) if cups_of_milk else "empty")}')
